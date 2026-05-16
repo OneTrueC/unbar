3c8232fe3581a9c82daa05516de7ff8c6111a0cb
Drew Marino <drewmarino25@gmail.com>
2026-05-16 16:01:59 -0400

    some architecting of how i would like window system interactions to work

---
+++ ws.h
@@ -0,0 +1,8 @@
+/* function declarations for window system functions */
+#define _POSIX_C_SOURCE 200809L
+
+struct windowCtx;
+typedef struct windowCtx windowCtx;
+
+/* return windowCtx type pointer (defined per ws) */
+windowCtx* initWs(void);
---
+++ y.c
@@ -0,0 +1,8 @@
+/* function declarations for wayland functions */
+#define _POSIX_C_SOURCE 200809L
+
+#include "ws.h"
+
+struct windowCtx {
+	//@TODO
+};
