01ed28e99cfdd6127b0ad3f95ff89e1bda74938b
CadetBluePaper <arknt8099@gmail.com>
2026-05-18 22:50:39 -0400

    context and bar structs

--- ws/y.c
+++ ws/y.c
@@ -1,8 +1,34 @@
 /* function declarations for wayland functions */
+#include <wayland-client-core.h>
+#include <wayland-client-protocol.h>
 #define _POSIX_C_SOURCE 200809L
 
+#include <wayland-client.h>
+#include "util.h"
 #include "ws.h"
 
-struct windowCtx {
-	//@TODO
+struct Bar {
+    struct wl_output* output;
+    struct wl_surface* surface;
+    struct zwlr_layer_surface_v1* layer;
+    int height;
+    int width;
+    int configured;
 };
+
+struct WindowCtx {
+	struct wl_display* dpy;
+	struct wl_registry* registry;
+
+	struct wl_compositor* compositor;
+	struct wl_shm* shm;
+	struct zwlr_layer_shell_v1* layer_shell;
+
+	int nbars;
+	struct Bar* bars;
+};
+
+WindowCtx*
+initWS(void) {
+
+}
